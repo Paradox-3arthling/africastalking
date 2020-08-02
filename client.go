2df2d8c2dac91769596aba2e56f0af8913d29d02
paradox-3arthling <floydqaranja@gmail.com>
2020-08-02 13:34:52 +0300

    Added a function for creating a request for clients since it's common in the API

--- client.go
+++ client.go
@@ -1,6 +1,11 @@
 package africastalking
 
-import "strings"
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"strings"
+)
 
 func SetUrl(prod bool, url string) string {
 	if prod {
@@ -8,4 +13,17 @@ func SetUrl(prod bool, url string) string {
 	} else {
 		return url
 	}
+}
+
+// BasicPostClient/1 - Created client will be dealing with
+// json only.
+func JsonRequest(url, api_key string, data []byte) (*http.Request, error) {
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
+	if err != nil {
+		return req, fmt.Errorf("'http.NewRequest/3' got the error: %q", err)
+	}
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("apiKey", api_key)
+	return req, nil
 }
