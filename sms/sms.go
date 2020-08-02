41771a18011afd31a9ddfaffd03cb2c8916ad5ba
paradox-3arthling <floydqaranja@gmail.com>
2020-08-02 14:08:32 +0300

    A working version though 'content-Type' for json is not supported unlike what is said in the docs

--- sms/sms.go
+++ sms/sms.go
@@ -11,25 +11,42 @@ import (
 
 const SMS_URL = "https://api.sandbox.africastalking.com/version1/messaging"
 
+// check how `json.Marshal/1` marshals arrays
 type Request_data struct {
-	Username             string `json:"username"`
-	To                   string `json:"to"`
-	Message              string `json:"message"`
-	From                 string `json:"from"`
-	BulkSMSMode          string `json:"bulkSMSMode"`
-	Enqueue              string `json:"enqueue"`
-	Keyword              string `json:"keyword"`
-	LinkId               string `json:"linkId"`
-	RetryDurationInHours string `json:"retryDurationInHours"`
+	Username             string   `json:"username"`
+	To                   []string `json:"to"`
+	Message              string   `json:"message"`
+	From                 string   `json:"from"`
+	BulkSMSMode          string   `json:"bulkSMSMode"`
+	Enqueue              string   `json:"enqueue"`
+	Keyword              string   `json:"keyword"`
+	LinkId               string   `json:"linkId"`
+	RetryDurationInHours string   `json:"retryDurationInHours"`
 }
 
-// func (req_data *Request_data) ConfirmFields() error {
-// 	if req_data
-// 	return nil
-// }
+// Need to confirm that the nos. are valid
+func (req_data *Request_data) ConfirmFields() error {
+	if req_data.Username == "" {
+		return fmt.Errorf("`ConfirmFields` got `req_data.Username` is blank!")
+	}
+	if len(req_data.To) == 0 {
+		return fmt.Errorf("`ConfirmFields` got `req_data.To` has no numbers to send to!")
+	}
+	if req_data.Message == "" {
+		return fmt.Errorf("`ConfirmFields` got `req_data.Message` is blank!")
+	}
+	return nil
+}
 
 // Return data at final func
 func (req_data *Request_data) SendSMS(prod bool) error {
+	if prod == false {
+		req_data.Username = "sandbox"
+	}
+	err := req_data.ConfirmFields()
+	if err != nil {
+		return fmt.Errorf("'req_data.ConfirmFields/0' got the error: %q", err)
+	}
 	data, err := json.Marshal(req_data)
 	if err != nil {
 		return fmt.Errorf("'json.Marshal/1' got the error: %q", err)
