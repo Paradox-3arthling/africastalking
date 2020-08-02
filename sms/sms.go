096171c7af2aa1d8371c16bf6f2d6818f426f964
paradox-3arthling <floydqaranja@gmail.com>
2020-08-02 13:44:10 +0300

    Formatted the data incase of anything to the proper case in the endpoint also inserted data to the client

--- sms/sms.go
+++ sms/sms.go
@@ -1,6 +1,7 @@
 package sms
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -11,15 +12,15 @@ import (
 const SMS_URL = "https://api.sandbox.africastalking.com/version1/messaging"
 
 type Request_data struct {
-	Username             string
-	To                   string
-	Message              string
-	From                 string
-	BulkSMSMode          string
-	Enqueue              string
-	Keyword              string
-	LinkId               string
-	RetryDurationInHours string
+	Username             string `json:"username"`
+	To                   string `json:"to"`
+	Message              string `json:"message"`
+	From                 string `json:"from"`
+	BulkSMSMode          string `json:"bulkSMSMode"`
+	Enqueue              string `json:"enqueue"`
+	Keyword              string `json:"keyword"`
+	LinkId               string `json:"linkId"`
+	RetryDurationInHours string `json:"retryDurationInHours"`
 }
 
 // func (req_data *Request_data) ConfirmFields() error {
@@ -29,8 +30,12 @@ type Request_data struct {
 
 // Return data at final func
 func (req_data *Request_data) SendSMS(prod bool) error {
+	data, err := json.Marshal(req_data)
+	if err != nil {
+		return fmt.Errorf("'json.Marshal/1' got the error: %q", err)
+	}
 	url := africastalking.SetUrl(prod, SMS_URL)
-	req, err := africastalking.JsonRequest(url, "", []byte{})
+	req, err := africastalking.JsonRequest(url, "", data)
 	if err != nil {
 		return fmt.Errorf("'africastalking.JsonRequest/3' got the error: %q", err)
 	}
