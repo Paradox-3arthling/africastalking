464b13c91ef97064bc07b4dc85c51aa1fb17d2bc
Paradox-3arthling <floydqaranja@gmail.com>
2020-08-23 20:00:06 +0300

    Fully fledged fetching of SMS's

--- sms/fetch_sms.go
+++ sms/fetch_sms.go
@@ -10,27 +10,24 @@ import (
 )
 
 func (req_data *Request_data) CheckInbox(last_received int) (map[string]interface{}, error) {
+	inbox := make(map[string]interface{})
 	if req_data.Prod == false {
 		req_data.Username = "sandbox"
 	}
-	inbox := make(map[string]interface{})
-	// !!change appropiately when the parameters come in
-	// https://api.sandbox.africastalking.com/version1/messaging?username=MyAppUsername&lastReceivedId=0
+	if req_data.Username == "" {
+		return inbox, fmt.Errorf("`CheckInbox/1` found `req_data.Username` is blank!")
+	}
+	if req_data.Api_key == "" {
+		return inbox, fmt.Errorf("`CheckInbox/1` found `req_data.Api_key` is blank!")
+	}
 	params := fmt.Sprintf("?username=%s&lastReceivedId=%v", req_data.Username, last_received)
-	// params := fmt.Sprintf("?username=floyd&lastReceivedId=0")
-	// !!change the url when the we whether we are in production or nah
 	url := africastalking.SetUrl(req_data.Prod, africastalking.INBOX_URL) + params
-	// !!delete this later
-	fmt.Printf("url:%q\n", url)
-	// url := africastalking.INBOX_URL + params
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return inbox, fmt.Errorf("'http.NewRequest/3' got the error: %q", err)
 	}
 	req.Header.Add("Accept", "application/json")
-	// !!set the API key
 	req.Header.Add("apiKey", req_data.Api_key)
-	// req.Header.Add("apiKey", "")
 
 	resp, err := client.Do(req)
 	if err != nil {
