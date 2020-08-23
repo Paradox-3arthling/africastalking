b40c5c8eae55c2b1af0a5a911075e4d266cdb8ac
Paradox-3arthling <floydqaranja@gmail.com>
2020-08-23 19:40:37 +0300

    merged the sms struct since it has all the basic fields needed

--- sms/fetch_sms.go
+++ sms/fetch_sms.go
@@ -9,23 +9,25 @@ import (
 	"github.com/paradox-3arthling/africastalking"
 )
 
-func CheckInbox() (map[string]interface{}, error) {
+func (req_data *Request_data) CheckInbox(last_received int) (map[string]interface{}, error) {
 	inbox := make(map[string]interface{})
 	// !!change appropiately when the parameters come in
 	// https://api.sandbox.africastalking.com/version1/messaging?username=MyAppUsername&lastReceivedId=0
-	// params := fmt.Sprintf("?username=%s&lastReceivedId=%v", username, last_received)
-	params := fmt.Sprintf("?username=floyd&lastReceivedId=0")
+	params := fmt.Sprintf("?username=%s&lastReceivedId=%v", req_data.Username, last_received)
+	// params := fmt.Sprintf("?username=floyd&lastReceivedId=0")
 	// !!change the url when the we whether we are in production or nah
-	// url := africastalking.SetUrl(prod, africastalking.INBOX_URL)
-	url := africastalking.INBOX_URL + params
+	url := africastalking.SetUrl(req_data.Prod, africastalking.INBOX_URL) + params
+	// !!delete this later
+	fmt.Printf("url:%q", url)
+	// url := africastalking.INBOX_URL + params
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return inbox, fmt.Errorf("'http.NewRequest/3' got the error: %q", err)
 	}
 	req.Header.Add("Accept", "application/json")
 	// !!set the API key
-	// req.Header.Add("apiKey", api_key)
-	req.Header.Add("apiKey", "")
+	req.Header.Add("apiKey", req_data.Api_key)
+	// req.Header.Add("apiKey", "")
 
 	resp, err := client.Do(req)
 	if err != nil {
